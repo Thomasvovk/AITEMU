d2c324f911d756a572b483be4993ebfc1583f393
Thomas Vovk <127060122+Thomasvovk@users.noreply.github.com>
2023-06-04 20:49:10 -0400

    First commit

---
+++ src/pages/utilities/apiPage.js
@@ -0,0 +1,5 @@
+export const apiKey = "fd5e01bfaeee4bcc905db6d589957270";
+
+export const apiUrlallGames = "https://api.rawg.io/api/games";
+
+export const apiAccess = `${apiUrlallGames}?key=${apiKey}`;
