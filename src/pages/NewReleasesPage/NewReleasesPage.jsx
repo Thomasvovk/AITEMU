254669f9a47f59c03fcd6b0a5351726cea1906d8
Thomas Vovk <127060122+Thomasvovk@users.noreply.github.com>
2023-06-17 01:18:47 -0400

    connected api to all the pages

--- src/pages/NewReleasesPage/NewReleasesPage.jsx
+++ src/pages/NewReleasesPage/NewReleasesPage.jsx
@@ -1,23 +1,39 @@
 import "../NewReleasesPage/NewReleasesPage.scss";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { apiRecentGamesReleases } from "../Utilities/API";
 import Card from "../../components/Card/Card";
 import axios from "axios";
-import { apiRecentGamesReleases } from "../Utilities/API";
 
 function NewReleasesPage() {
-  // const [recentGames, setRecentGames] = useState ([]);
+  const [newReleases, setnewReleases] = useState([]);
+  const arr = [];
 
-  // useEffect(()=> {
-  //     axios
-  //     .get(apiRecentGamesReleases)
-  //     .then((response) => {
-  //         setRecentGames(response.data)
-  //     })
-  // })
+  useEffect(() => {
+    axios.get(apiRecentGamesReleases).then((response) => {
+      arr.push(response.data.results);
+      setnewReleases(response.data.results);
+    });
+  }, []);
+
+  console.log(newReleases);
 
   return (
     <>
-      <h1 className="new-release">New Releases </h1>
+      <h1 className="home__buttom-title">New Releases</h1>
+      <div className="home__games">
+        {newReleases.map((item) => {
+          return (
+            <Link to={`/new-releases`}>
+              <Card
+                name={item.name}
+                image={item.background_image}
+                // id={item.id}
+              />
+            </Link>
+          );
+        })}
+      </div>
     </>
   );
 }
--- src/pages/Utilities/API.jsx
+++ src/pages/Utilities/API.jsx
@@ -1,15 +1,14 @@
-export const apiKey = "35a4c87e8d814732916ac9e38857a151";
+export const apiKey = "6aaacae876fa4406b745b78f6024343a";
 export const apiUrlGames = "https://api.rawg.io/api/games";
 export const apiUrlPlatforms = "https://api.rawg.io/api/platforms";
 export const apiUrlPublisher = "https://api.rawg.io/api/publishers";
 export const queryMetacritic = "metacritic=90-100";
-export const pageSize = "page_size=50";
-export const recentGamesReleases = "dates=30days";
+export const pageSize = "page_size=200";
+export const recentGamesReleases = "dates=2023-05-01,2023-05-31";
 
 export const apiGameID = `${apiUrlGames}/:id?&key=${apiKey}`;
 export const apiAllGamesMetacritics = `${apiUrlGames}?${queryMetacritic}&${pageSize}&key=${apiKey}`;
 export const apiRecentGamesReleases = `${apiUrlGames}?${recentGamesReleases}&key=${apiKey}`;
 export const apiAllPlatforms = `${apiUrlPlatforms}?key=${apiKey}`;
 export const apiAllPublishers = `${apiUrlPublisher}?key=${apiKey}`;
-
 export const apiPlatforms = `${apiUrlPlatforms}?${pageSize}$key=${apiKey}`;
