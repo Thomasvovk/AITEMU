e4c21b2eea5d48a47754452e6166cde8b3386f9d
Thomas Vovk <127060122+Thomasvovk@users.noreply.github.com>
2023-06-17 22:33:40 -0400

    (added info/vector to Platforms Page)

--- src/pages/PlatformsPage/PlatformsPage.jsx
+++ src/pages/PlatformsPage/PlatformsPage.jsx
@@ -18,8 +18,20 @@ function Platforms() {
   }, []);
   return (
     <>
-      <h1 className="home__buttom-title">Platforms</h1>
-      <div className="home__games">
+      <h1 className="platforms__buttom-title">Platforms</h1>
+      <div className="platforms__character-container">
+        <p className="platforms__character-subheader">
+          Welcome to our Top Metacritic page! Discover the highest-rated games
+          across genres and platforms that have captivated both critics and
+          players. Explore captivating worlds, engage in strategic battles, and
+          indulge in cinematic storytelling. Join our community and experience
+          gaming excellence at its finest.
+        </p>
+
+        <div className="platforms__character-right"></div>
+      </div>
+
+      <div className="platforms__games">
         {allPlatforms.map((item) => {
           return (
             <Link to={`/platforms`}>
