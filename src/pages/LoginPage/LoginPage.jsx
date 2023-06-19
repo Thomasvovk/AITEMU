10c4b928b0e269ed5b527e420bd57014cef89e7b
Thomas Vovk <127060122+Thomasvovk@users.noreply.github.com>
2023-06-19 13:58:01 -0400

    (added structure for sign in & sign out page + added style to both and connect by using Link )

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,28 +1,60 @@
 import "../LoginPage/LoginPage.scss";
+import { Link } from "react-router-dom";
 
 function LoginPage() {
   return (
     <>
       <div className="log-in">
         <div className="log-in__image"></div>
-        <div>
+        <div className="log-in__credentials-contaiter">
           <h1 className="log-in__title">Welcome to AITEMU </h1>
           <h2 className="log-in__subheader">Sign In to Continue</h2>
           <p className="log-in__description">
             Don't have an account? Create a account It takes less than a minute.
           </p>
           <form action="" method="get">
-            <div>
-              <label for="name">Email </label>
-              <input type="text" name="name" id="name" required />
+            <div className="log-in__auth-container">
+              <label className="log-in__container-title" for="name">
+                Email{" "}
+              </label>
+              <input
+                className="log-in__container-input"
+                type="text"
+                name="name"
+                placeholder="Please enter your email"
+                id="name"
+                required
+              />
             </div>
-            <div>
-              <label for="email">Password </label>
-              <input type="email" name="email" id="email" required />
+            <div className="log-in__auth-container">
+              <label className="log-in__container-title" for="email">
+                Password{" "}
+              </label>
+              <input
+                className="log-in__container-input"
+                type="email"
+                name="email"
+                placeholder="Please enter your password"
+                id="email"
+                required
+              />
             </div>
-            <div>
-              <input type="submit" value="Sign in" />
+            <Link className="log-in__forgot-link" to="#">
+              <p className="log-in__forgot-password">Forgot Password?</p>
+            </Link>
+            <div className="log-in__button-container">
+              <input className="log-in__button" type="submit" value="Sign in" />
             </div>
+
+            <Link to="/sign-up">
+              <div className="log-in__signup-container">
+                <input
+                  className="log-in__signup-button"
+                  type="submit"
+                  value="Sign up"
+                />
+              </div>
+            </Link>
           </form>
         </div>
       </div>
--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,9 +1,57 @@
 import "../SignupPage/SignupPage.scss";
+import { Link } from "react-router-dom";
 
 function SignUpPage() {
   return (
     <>
-      <div></div>
+      <div className="sign-up">
+        <div className="sign-up__image"></div>
+        <div className="sign-up__credentials-contaiter">
+          <h1 className="sign-up__title">Welcome to AITEMU </h1>
+          <h2 className="sign-up__subheader">Sign In to Continue</h2>
+          <p className="sign-up__description">
+            Don't have an account? Create a account It takes less than a minute.
+          </p>
+          <form action="" method="get">
+            <div className="sign-up__auth-container">
+              <label className="sign-up__container-title" for="name">
+                Email{" "}
+              </label>
+              <input
+                className="sign-up__container-input"
+                type="text"
+                name="name"
+                placeholder="Please enter your email"
+                id="name"
+                required
+              />
+            </div>
+            <div className="sign-up__auth-container">
+              <label className="sign-up__container-title" for="email">
+                Password{" "}
+              </label>
+              <input
+                className="sign-up__container-input"
+                type="email"
+                name="email"
+                placeholder="Please enter your password"
+                id="email"
+                required
+              />
+            </div>
+            <Link className="sign-up__forgot-link" to="/login">
+              <p className="sign-up__forgot-password">Already a user?</p>
+            </Link>
+            <div className="sign-up__button-container">
+              <input
+                className="sign-up__button"
+                type="submit"
+                value="Sign Up"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
